590a648e0eee27e01608d6f9932787106f8d792c
Joerlan.Joe@gmail.com <Joerlan.Joe@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-08-22 15:57:52 +0000

    retirado import pra nao ficar warning

--- SOS-Web/src/br/edu/fanor/manager/loginManeger.java
+++ SOS-Web/src/br/edu/fanor/manager/loginManeger.java
@@ -3,7 +3,6 @@ package br.edu.fanor.manager;
 import java.io.IOException;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.application.NavigationCase;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
