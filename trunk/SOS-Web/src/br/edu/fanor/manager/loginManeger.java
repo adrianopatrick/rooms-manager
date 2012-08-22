b650abfa428efe44ce97606ce82d4d561a7a4eeb
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-08-22 14:04:17 +0000

    Exemplo de validação - Login

---
+++ trunk/SOS-Web/src/br/edu/fanor/manager/loginManeger.java
@@ -0,0 +1,54 @@
+package br.edu.fanor.manager;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.NavigationCase;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.context.RequestContext;
+
+@ManagedBean
+public class loginManeger {
+
+	private String email;
+
+	private String senha;
+
+	public void login(ActionEvent actionEvent) throws IOException {
+		RequestContext context = RequestContext.getCurrentInstance();
+		FacesMessage msg = null;
+		boolean loggedIn = false;
+
+		if (email != null && email.equals("admin") && senha != null && senha.equals("admin")) {
+			loggedIn = true;
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome muito maxo!!", email);
+			FacesContext.getCurrentInstance().getExternalContext().redirect("paginas/homeProfessor.xhtml");  
+		} else {
+			loggedIn = false;
+			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Errou doido", "Invalido mermao");
+		}
+
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		context.addCallbackParam("loggedIn", loggedIn);
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
