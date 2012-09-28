99f8fd6bb1c5002b927cda86cfdbe66c960847cc
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-09-28 02:18:58 +0000

    sem comentários de codigo

--- SOS-Web/src/br/edu/fanor/manager/loginManeger.java
+++ SOS-Web/src/br/edu/fanor/manager/loginManeger.java
@@ -17,59 +17,48 @@ import br.edu.fanor.service.LoginService;
 @ManagedBean
 public class loginManeger {
  
-	private String email;
+        private String email;
 
-	private String senha;
-	
-	@EJB
-	private LoginService loginService;
-	
-	private Usuario usuario;
+        private String senha;
+        
+        @EJB
+        private LoginService loginService;
+        
+        private Usuario usuario;
+        
+        public void login(ActionEvent actionEvent) throws IOException {
+                RequestContext context = RequestContext.getCurrentInstance();
+                FacesMessage msg = null;
+                boolean loggedIn = false;
+                
+                try {
+                        usuario = loginService.validaLogin(email, senha);
+                        loggedIn = true;
+                        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", usuario);
+                        FacesContext.getCurrentInstance().getExternalContext().redirect("homeProfessor.jsf");
+                } catch (DefaultException e) {
+                        msg = new FacesMessage(FacesMessage.SEVERITY_WARN, e.getMsg(), "");
+                        FacesContext.getCurrentInstance().addMessage(null, msg);
+                }
+                
+                context.addCallbackParam("loggedIn", loggedIn);
+                
+        }
 
-	public void login(ActionEvent actionEvent) throws IOException {
-		RequestContext context = RequestContext.getCurrentInstance();
-		FacesMessage msg = null;
-		boolean loggedIn = false;
-		
-		try {
-			usuario = loginService.validaLogin(email, senha);
-			loggedIn = true;
-			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", usuario);
-			FacesContext.getCurrentInstance().getExternalContext().redirect("homeProfessor.jsf");
-		} catch (DefaultException e) {
-			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, e.getMsg(), "");
-//			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login ou senha Incorretos", "");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		}
-		
-		context.addCallbackParam("loggedIn", loggedIn);
-//		if (email != null && email.equals(usuario.getEmail()) && senha != null && senha.equals(usuario.getSenha())) {
-//			loggedIn = true;
-////			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bem Vindo!!", email);
-//			FacesContext.getCurrentInstance().getExternalContext().redirect("homeProfessor.jsf");  
-//		} else {
-//			loggedIn = false;
-//			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login ou senha Incorretos", "");
-//		}
-//		
-//		FacesContext.getCurrentInstance().addMessage(null, msg);
-		
-	}
+        public String getEmail() {
+                return email;
+        }
 
-	public String getEmail() {
-		return email;
-	}
+        public void setEmail(String email) {
+                this.email = email;
+        }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+        public String getSenha() {
+                return senha;
+        }
 
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+        public void setSenha(String senha) {
+                this.senha = senha;
+        }
 
 }
