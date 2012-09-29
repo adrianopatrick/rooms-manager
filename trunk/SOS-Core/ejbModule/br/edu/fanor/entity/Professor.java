65d31d863475d9d666ecad7062a01dbce7ff4ce9
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-09-29 16:51:54 +0000

    redirecionando e testes

--- trunk/SOS-Core/ejbModule/br/edu/fanor/entity/Professor.java
+++ trunk/SOS-Core/ejbModule/br/edu/fanor/entity/Professor.java
@@ -3,8 +3,6 @@ package br.edu.fanor.entity;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 
 @Entity(name="professores")
