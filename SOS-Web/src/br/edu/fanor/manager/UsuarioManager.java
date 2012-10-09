b573971b2ee6a95bb427d6462ddfe7e5598d7d2b
maciel.dev@gmail.com <maciel.dev@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-09 06:35:40 +0000

    Toda a implementação do CRUD de usuário está na classe "UsuarioManager"... (acredito que essa deveria ser UsuarioControler, em fim...)
    Na classe foi utilizado os métodos já existentes no GenericDAO, mas ainda carece de revisão e testes
    
    Na classe SessionUtil.java eu criei uma espécie de fábrica de conexão, mas não cheguei a usar porque o mesmo já é implementando em GenericDAO através do EntityManager.
    
    Nas demais classes só tem alguns comentários, nada de importante... era apenas eu lendo para entende-las.

--- SOS-Web/src/br/edu/fanor/manager/UsuarioManager.java
+++ SOS-Web/src/br/edu/fanor/manager/UsuarioManager.java
@@ -1,5 +1,6 @@
 package br.edu.fanor.manager;
 
+//import java.util.List;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
@@ -7,6 +8,7 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
+import br.edu.fanor.dao.UsuarioDAO;
 import br.edu.fanor.entity.Usuario;
 
 @SessionScoped
@@ -19,7 +21,13 @@ public class UsuarioManager implements Serializable{
 	
 	private Usuario usuario;
 	
+//	private Class<Usuario> usuarios;
+	
+		
+	//TODO realmente não é necessáiro um construtor?
+	
 	public Usuario getUsuario() {
+		//TODO verificar necessidade de checagem
 		return usuario;
 	}
 	
@@ -33,7 +41,48 @@ public class UsuarioManager implements Serializable{
 	}
 
 	private HttpServletRequest getRequest() {
-		return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		return (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
 	}
+	
+	public String salvar(Usuario usuario){
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		
+		usuarioDAO.insert(usuario);
+		
+		//TODO para qual página redirecionar?
+		return "";
+	}
+	
+	public String deletar(Usuario usuario){
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.delete(usuario);
+		
+		//TODO para qual página redirecionar?
+		return "";
+	}
+	
+	public String atualizar(Usuario usuario){
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		
+		usuarioDAO.insertOrUpdate(usuario);
+		
+		//TODO para qual página redirecionar?
+		return "";
+	}
+	
+	 public Usuario findByEmail(){
+		 UsuarioDAO usuarioDAO = new UsuarioDAO();
+		 usuarioDAO.findByEmail(usuario.getEmail());
+		 
+		 return usuario;
+	 }
+	
+	 //TODO criar uma classe para paginação
+//	 public List<Usuario> findAll(){
+//		 UsuarioDAO usuarioDAO = new UsuarioDAO();
+//		 return usuarioDAO.findAll(usuarios, true);
+//	 }
+	
 	
 }
