be8f43f4b051f7ccc31f89ec2310c0d1530c9b9c
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-10 11:42:16 +0000

    organizando

--- SOS-Web/WebContent/Documentos/BancoDeDados/inserts.sql
+++ SOS-Web/WebContent/Documentos/BancoDeDados/inserts.sql
@@ -2,9 +2,9 @@
 insert into perfis_usuario values (2, 'professores');
 insert into perfis_usuario values (3, 'funcionarios');
 
-insert into usuarios values ('administradores', 1, 'h2milhome', 'Herbeth', '123');
-insert into usuarios values ('professores', 2, 'joe', 'Joerlan', '123');
-insert into usuarios values ('funcionarios', 3, 'tiago@', 'Tiago', '123');
+insert into usuarios values ('administradores', 1, 'admin', 'Administrador', 'admin');
+insert into usuarios values ('professores', 2, 'prof', 'Professor', 'prof');
+insert into usuarios values ('funcionarios', 3, 'func', 'Funcionario', 'func');
 
 insert into perfilusuario_usuario values (1, 1);
 insert into perfilusuario_usuario values (2, 2);
