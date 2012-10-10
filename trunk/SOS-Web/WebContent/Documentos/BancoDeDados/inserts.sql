b650a8912101492f13884b1fa6b345cc87ce833f
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-10 17:47:20 +0000

    inserts

--- trunk/SOS-Web/WebContent/Documentos/BancoDeDados/inserts.sql
+++ trunk/SOS-Web/WebContent/Documentos/BancoDeDados/inserts.sql
@@ -1,11 +1,21 @@
-﻿﻿insert into perfis_usuario values (1, 'administradores');
-insert into perfis_usuario values (2, 'professores');
-insert into perfis_usuario values (3, 'funcionarios');
+INSERT INTO perfis_admins VALUES (1, 'administradores');
+INSERT INTO perfis_admins VALUES (2, 'professores');
 
-insert into usuarios values ('administradores', 1, 'admin', 'Administrador', 'admin');
-insert into usuarios values ('professores', 2, 'prof', 'Professor', 'prof');
-insert into usuarios values ('funcionarios', 3, 'func', 'Funcionario', 'func');
+INSERT INTO usuarios VALUES ('administradores', 1, 'admin', 'Administrador', 'admin');
+INSERT INTO usuarios VALUES ('professores', 2, 'prof', 'Professor', 'prof');
+INSERT INTO usuarios VALUES ('funcionarios', 3, 'func', 'Funcionario', 'func');
 
-insert into perfilusuario_usuario values (1, 1);
-insert into perfilusuario_usuario values (2, 2);
-insert into perfilusuario_usuario values (3, 3);
+INSERT INTO permissoes VALUES (1, 'salvar', true);
+INSERT INTO permissoes VALUES (2, 'editar', true);
+INSERT INTO permissoes VALUES (3, 'atualizar', true);
+INSERT INTO permissoes VALUES (4, 'excluir', true);
+
+<-- INSERT INTO perfis_admins_permissoes VALUES (1, 1);
+<-- INSERT INTO perfis_admins_permissoes VALUES (1, 2);
+<-- INSERT INTO perfis_admins_permissoes VALUES (1, 3);
+<-- INSERT INTO perfis_admins_permissoes VALUES (1, 4);
+<-- INSERT INTO perfis_admins_permissoes VALUES (2, 1);
+<-- INSERT INTO perfis_admins_permissoes VALUES (2, 2);
+<-- INSERT INTO perfis_admins_permissoes VALUES (3, 1);
+<-- INSERT INTO perfis_admins_permissoes VALUES (3, 2);
+<-- INSERT INTO perfis_admins_permissoes VALUES (3, 3);
