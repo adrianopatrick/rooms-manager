ac8d32442965645d66f5bc86e6dba3e721593358
maciel.dev@gmail.com <maciel.dev@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-13 18:17:50 +0000

    1- Correção de UsuárioManager; 2- UsuárioService criado para respeitar a arquitetura do sistema (e tirar a feiura do código hehehe)
    Faltam melhorias e implementar 2 métodos em Service

--- trunk/SOS-Core/ejbModule/br/edu/fanor/service/UsuarioService.java
+++ trunk/SOS-Core/ejbModule/br/edu/fanor/service/UsuarioService.java
@@ -3,6 +3,7 @@ package br.edu.fanor.service;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import antlr.collections.List;
 import br.edu.fanor.dao.UsuarioDAO;
 import br.edu.fanor.entity.Usuario;
 
@@ -12,13 +13,39 @@ public class UsuarioService extends GenericService<Usuario>{
 	private static final long serialVersionUID = 5218604509449444278L;
 	
 	@EJB
-	private UsuarioDAO dao;
+	private UsuarioDAO usuarioDAO;
+	
+	private List usuariosList;
 
 	public Usuario findByEmail(String email) {
-		Usuario usuario = dao.findByEmail(email);
+		Usuario usuario = usuarioDAO.findByEmail(email);
 		return usuario;
 	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public void salvar(Usuario usuario){
+		usuarioDAO.insert(usuario);
+	}
 	
+	public void deletar(Usuario usuario){
+		usuarioDAO.delete(usuario);	
+	}
+	
+	public void atualizar(Usuario usuario){
+			usuarioDAO.insertOrUpdate(usuario);	
+	}
+		
+	 //TODO terminar metodos e criar paginação
+	public List getUsuariosList() {
+		return usuariosList;
+	}
+
+	public void setUsuariosList(List usuariosList) {
+		this.usuariosList = usuariosList;
+	}
 }
 
 
