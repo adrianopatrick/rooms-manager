169deb3aa13354ebddec9598a741d1000dd4e1cf
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-13 06:43:45 +0000

    lista solicitacao

--- SOS-Web/src/br/edu/fanor/manager/homeProfessorManager.java
+++ SOS-Web/src/br/edu/fanor/manager/homeProfessorManager.java
@@ -1,6 +1,7 @@
 package br.edu.fanor.manager;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -8,7 +9,8 @@ import javax.faces.bean.SessionScoped;
 
 import br.edu.fanor.entity.Reserva;
 import br.edu.fanor.entity.Solicitacao;
-import br.edu.fanor.service.EstadoReservaService;
+import br.edu.fanor.service.ReservaService;
+import br.edu.fanor.service.SolicitacaoService;
 
 @SessionScoped
 @ManagedBean
@@ -17,7 +19,10 @@ public class homeProfessorManager implements Serializable{
 	private static final long serialVersionUID = -7652074045374161311L;
 
 	@EJB
-	EstadoReservaService reservaService;
+	SolicitacaoService solicitacaoService;
+	
+	@EJB
+	ReservaService reservaService;
 	
 	private Reserva reserva = new Reserva();
 	private Solicitacao solicitacao = new Solicitacao();
@@ -25,6 +30,11 @@ public class homeProfessorManager implements Serializable{
 	public String salvar(){
 		reservaService.salvaReserva(reserva, solicitacao);
 		return "homeProfessor";
+	}
+	
+	public List<Solicitacao> listaSolicitacao(){
+		List<Solicitacao> lista = solicitacaoService.listSolicitacao();
+		return lista;
 	}
 	
 	public Solicitacao getSolicitacao() {
