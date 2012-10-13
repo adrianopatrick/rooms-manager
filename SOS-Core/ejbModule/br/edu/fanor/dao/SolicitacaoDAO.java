2185caa9a8bab29a8814ede3014cd0efeec8fc33
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-13 17:07:25 +0000

    solicitacao e reserva

--- SOS-Core/ejbModule/br/edu/fanor/dao/SolicitacaoDAO.java
+++ SOS-Core/ejbModule/br/edu/fanor/dao/SolicitacaoDAO.java
@@ -1,6 +1,10 @@
 package br.edu.fanor.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+
+import org.hibernate.Criteria;
 
 import br.edu.fanor.entity.Solicitacao;
 
@@ -8,5 +12,11 @@ import br.edu.fanor.entity.Solicitacao;
 public class SolicitacaoDAO extends GenericDAO<Solicitacao>{
 
 	private static final long serialVersionUID = -2937516627590134125L;
+
+	@SuppressWarnings("unchecked")
+	public List<Solicitacao> findAll() {
+		Criteria criteria = getCriteria(Solicitacao.class);
+		return criteria.list();
+	}
 
 }
---
+++ SOS-Core/ejbModule/br/edu/fanor/service/SolicitacaoService.java
@@ -0,0 +1,29 @@
+package br.edu.fanor.service;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import br.edu.fanor.dao.SolicitacaoDAO;
+import br.edu.fanor.entity.Solicitacao;
+import br.edu.fanor.entity.Usuario;
+
+@Stateless
+public class SolicitacaoService extends GenericService<Usuario>{
+
+	private static final long serialVersionUID = -8469735845097835531L;
+
+	@EJB
+	SolicitacaoDAO solicitacaoDAO;
+
+	public void salvaSolicitacao(Solicitacao solicitacao) {
+		solicitacaoDAO.insert(solicitacao);
+	}
+	
+	public List<Solicitacao> listSolicitacao(){
+		List<Solicitacao> list = solicitacaoDAO.findAll();
+		return list;
+	}
+
+}
