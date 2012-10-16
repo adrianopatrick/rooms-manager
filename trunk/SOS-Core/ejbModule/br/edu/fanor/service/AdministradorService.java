5b925781e5a4a8a4db40d6bedf7da7bc3365af85
maciel.dev@gmail.com <maciel.dev@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-16 21:54:24 +0000

    Classes a serem trabalhadas.

---
+++ trunk/SOS-Core/ejbModule/br/edu/fanor/service/AdministradorService.java
@@ -0,0 +1,9 @@
+package br.edu.fanor.service;
+
+import br.edu.fanor.entity.Administrador;
+
+public class AdministradorService extends GenericService<Administrador> {
+
+	private static final long serialVersionUID = -3045197905744141478L;
+
+}
