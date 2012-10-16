3ad2aea6f658188f285f35420343531962a7166e
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-10-16 13:36:15 +0000

    corrigindo mensagens, para ver se sai os caracteres especiais. removi insert Prof. sequence nao esta aconpanhando. cadastro funcionario substituido por Usuário

--- SOS-Core/resource/bancoDados/inserts.sql
+++ SOS-Core/resource/bancoDados/inserts.sql
@@ -1,19 +1,7 @@
 INSERT INTO perfis_admins VALUES (1, 'master');
 
 INSERT INTO usuarios VALUES ('administradores', 1, 'admin', 'Administrador', 'admin', 1);
-INSERT INTO usuarios VALUES ('professores', 2, 'prof', 'Professor', 'prof');
 
 INSERT INTO permissoes VALUES (1, 2, true,1);
 INSERT INTO permissoes VALUES (2, 3, true,1);
 INSERT INTO permissoes VALUES (3, 1, true,1);
-
-
-<-- INSERT INTO perfis_admins_permissoes VALUES (1, 1);
-<-- INSERT INTO perfis_admins_permissoes VALUES (1, 2);
-<-- INSERT INTO perfis_admins_permissoes VALUES (1, 3);
-<-- INSERT INTO perfis_admins_permissoes VALUES (1, 4);
-<-- INSERT INTO perfis_admins_permissoes VALUES (2, 1);
-<-- INSERT INTO perfis_admins_permissoes VALUES (2, 2);
-<-- INSERT INTO perfis_admins_permissoes VALUES (3, 1);
-<-- INSERT INTO perfis_admins_permissoes VALUES (3, 2);
-<-- INSERT INTO perfis_admins_permissoes VALUES (3, 3);
