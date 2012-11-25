4f20e671ad11ecd3aabd205b7a959b0866a75f3d
h2milhome@gmail.com <h2milhome@gmail.com@2024e61c-712d-cc8a-2434-49abe59ee5e9>
2012-11-25 01:54:10 +0000

    correções de bugs

--- trunk/SOS-Core/resource/bancoDados/selects.sql
+++ trunk/SOS-Core/resource/bancoDados/selects.sql
@@ -1,3 +1,6 @@
 
 --seleciona as salas que nao estao reservadas na data 2012-11-07 das 06:00:00 as 07:30:00
 select * from salas s where s.id not in (select sala_id from reservas r where not (('2012-11-07 06:00:00' < r.data_inicial and '2012-11-07 07:30:00' <= r.data_inicial) or ('2012-11-07 06:00:00' >= r.data_final and '2012-11-07 07:30:00' > r.data_final)))
+
+--seleciona as solicitacoes que nao tem uma reserva associada
+select * from solicitacoes s where s.id not in (select r.solicitacao_id from reservas r where r.solicitacao_id = s.id)
